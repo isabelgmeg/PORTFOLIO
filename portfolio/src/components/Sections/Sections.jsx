b40883d12f9c448467fe06d146c92d5489f39e76
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-10-04 18:25:17 +0200

    added some more colors and styles

--- portfolio/src/components/Sections/Sections.jsx
+++ portfolio/src/components/Sections/Sections.jsx
@@ -1,24 +1,36 @@
 import DesignMainPic from "../../assets/design-main-pic.jpg";
 import DesignMiniPic1 from "../../assets/design-mini-pic1.jpg";
 import DesignMiniPic2 from "../../assets/design-mini-pic2.jpg";
-
+import Circle from "../../assets/circle.png";
 import "./Sections.scss";
 
 export default function Sections() {
   return (
     <div className="sections">
       <section className="design">
-        <h2 className="design__title">Design</h2>
+        <h2 className="design__title">design</h2>
+        <section className="design__decorative">
+          <img src={Circle} alt="decorative element"></img>
+        </section>
         <span className="design__ability">
-          <p className="design__ability__display">Designer</p>
+          <p className="design__ability__display">
+            <strong>Designer</strong>
+          </p>
           <span className="design__ability__line"></span>
           <p className="design__ability__soft">Programmer</p>
         </span>
         <p className="design__text">
-          t volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie c
+          for the past 8 years I have been working as a{" "}
+          <em>
+            <strong>GRAPHIC DESIGNER</strong>
+          </em>{" "}
+          AND{" "}
+          <em>
+            {" "}
+            <strong>design project manager </strong>
+          </em>
+          for Springfield, a retail company present in 68 countries with{" "}
+          <em>931 points of sale.</em>
         </p>
       </section>
       <section className="pics">
